ebc724ec6ab9622381efc73cdb06bc00b6d2bf44
ParkSangMin <tkdals5402@mutant.ai.kr>
2026-07-25 01:18:09 +0900

    Release 3.0.1 scene extensions

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,11 @@ All notable changes to this package will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [3.0.1] - 2026-07-25
+
+### Added
+- Added SceneExtensions for finding components within loaded Unity scenes.
+
 ## [3.0.0] - 2026-07-25
 
 ### Breaking Changes
---
+++ Runtime/Scripts/Extensions/SceneExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace com.parkminpackages.expansion.Extensions
+{
+	public static class SceneExtensions
+	{
+		public static T FindComponent<T>(this Scene scene, bool includeInactive = true)
+			where T : class {
+			if (!scene.IsValid() || !scene.isLoaded)
+				return null;
+
+			foreach (GameObject root in scene.GetRootGameObjects()) {
+				T component = root.GetComponentInChildren<T>(includeInactive);
+
+				if (component != null)
+					return component;
+			}
+
+			return null;
+		}
+
+		public static List<T> FindComponents<T>(this Scene scene, bool includeInactive = true)
+			where T : class {
+			List<T> results = new List<T>();
+
+			if (!scene.IsValid() || !scene.isLoaded)
+				return results;
+
+			foreach (GameObject root in scene.GetRootGameObjects())
+				root.GetComponentsInChildren(includeInactive, results);
+
+			return results;
+		}
+	}
+}
